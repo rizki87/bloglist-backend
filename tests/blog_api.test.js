26a7cc6ec656413a60264cefd6db390aa98d433e
rizki87 <rizkipratama87@gmail.com>
2021-02-28 19:13:25 +0700

    Exercise, 4.10: Blog list tests, step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const listHelper = require('../utils/list_helper')
 const app = require('../app')
 
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of listHelper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
 
 test('blogs are returned as json and have a certain number of blog posts', async () => {
     await api
@@ -18,6 +30,29 @@ test('field named id is exists', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid note can be added ', async () => {
+  const newBlog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await listHelper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(listHelper.initialBlogs.length + 1)
+
+  const title = blogsAtEnd.map(b => b.title)
+  expect(title).toContainEqual(
+    'Canonical string reduction'
+  )
 })
 
 afterAll(() => {
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,20 @@
 var _ = require('lodash')
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+        {
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7
+        },
+        {
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5
+        }
+]
 
 const dummy = (blogs) => {
   return blogs = 1
@@ -46,10 +62,17 @@ const mostLikes = (blogs) => {
         : theMostLikes
 }
 
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
 module.exports = {
+  initialBlogs,
   dummy,
   totalLikes,
   favoriteBlog,
   mostBlogs,
-  mostLikes
+  mostLikes,
+  blogsInDb
 }
